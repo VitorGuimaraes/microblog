3b2c283fc517b111d14daeac6f4c5c7114f1ea5f
Vitor Guimarães <vitorguimaraes.ce@gmail.com>
2021-04-27 21:47:22 -0300

    Chapter 2: Templates (v0.2)

--- microblog.py
+++ microblog.py
@@ -1,1 +1,9 @@
 from app import app
+
+# Antes de executar, insira no terminal:
+# export FLASK_APP=microblog.py 
+# flask run
+
+# Esse comando diz ao flask qual aplicação ele deve servir
+# Se não for informado, o wsgi.py é importado e tenta identificar
+# automaticamente um app (app) ou factory (create_app)
